79c7668facc3cc39aa18941c7a30c8fdfa6b5917
Guille <guillermobernallou@gmail.com>
2026-02-11 22:09:45 +0100

    feat: improve audio

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import yt_dlp
 url = input("Enter the YouTube video URL: ")
 
 ydl_opts = {
-    "format": "bestvideo+bestaudio/best",
+    "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best",
     "merge_output_format": "mp4",
 }
 
